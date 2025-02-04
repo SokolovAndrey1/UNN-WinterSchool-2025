97cce76129e287ce7332f835cc13729d1cb42f61
Daniil <reactive722@gmail.com>
2025-02-04 12:25:00 +0300

    #add abs_rvv realized

--- perf/include/perf_common.hpp
+++ perf/include/perf_common.hpp
@@ -57,10 +57,18 @@ static void printStat(uint64_t minCycles, uint64_t totalCycles, int iterations, 
 }
 
 #ifdef __riscv
-static inline unsigned long rdcycle(void) 
+// for BananaPi
+// static __inline __attribute__((__always_inline__, __artificial__)) unsigned long rdcycle(void) 
+// { 
+//     uint64_t dst; 
+//     asm volatile ("rdtime %0" : "=r" (dst) ); 
+//     return dst; 
+// }
+// for gem5
+static inline __attribute__((__always_inline__, __artificial__)) uint64_t rdcycle(void) 
 { 
     uint64_t dst; 
-    asm volatile ("rdtime %0" : "=r" (dst) ); 
+    asm volatile ("csrrs %0, 0xc00, x0" : "=r" (dst)); 
     return dst; 
 }
 #endif // __riscv
