6fb0a14b54f619e8632111a8f84bd7b4d080a14b
Daniil <reactive722@gmail.com>
2025-02-06 15:30:30 +0300

    #add alpha_compositing_rvv

--- lib/src/rvv/alpha_compositing.c
+++ lib/src/rvv/alpha_compositing.c
@@ -1,6 +1,43 @@
 #include "rvv.h"
 
-void alpha_compositing_rvv(uint8_t* foreground, uint8_t* background, float alpha, uint8_t* dst, int width, int height, int channels) {
-    // Just a placeholder
-    alpha_compositing_scalar(foreground, background, alpha, dst, width, height, channels);
+// LMUL 2-8
+// void alpha_compositing_rvv(uint8_t *foreground, uint8_t *background, float alpha, uint8_t *dst, int width, int height, int channels)
+// {
+//     int arr_size = width * height * channels;
+//     for (size_t vl; arr_size > 0; arr_size -= vl, foreground += vl, background += vl, dst += vl)
+//     {
+//         vl = __riscv_vsetvl_e32m8(arr_size);
+//         vuint16m4_t v_u16_foreground = __riscv_vwcvtu_x_x_v_u16m4(__riscv_vle8_v_u8m2(foreground, vl), vl);
+//         vuint16m4_t v_u16_background = __riscv_vwcvtu_x_x_v_u16m4(__riscv_vle8_v_u8m2(background, vl), vl);
+
+//         vfloat32m8_t v_f32_foreground = __riscv_vfwcvt_f_xu_v_f32m8(v_u16_foreground, vl);
+//         vfloat32m8_t v_f32_background = __riscv_vfwcvt_f_xu_v_f32m8(v_u16_background, vl);
+
+//         v_f32_foreground = __riscv_vfmul_vf_f32m8(v_f32_foreground, alpha, vl);
+//         vfloat32m8_t v_f32_dst = __riscv_vfmacc_vf_f32m8(v_f32_foreground, 1.0f - alpha, v_f32_background, vl);
+
+//         vuint16m4_t v_u16_dst = __riscv_vfncvt_xu_f_w_u16m4(v_f32_dst, vl);
+//         __riscv_vse8_v_u8m2(dst, __riscv_vncvt_x_x_w_u8m2(v_u16_dst, vl), vl); // v_u16_dst -> v_u8_dst and save in dst
+//     }
+// }
+
+// LMUL 1-4
+void alpha_compositing_rvv(uint8_t *foreground, uint8_t *background, float alpha, uint8_t *dst, int width, int height, int channels)
+{
+    int arr_size = width * height * channels;
+    for (size_t vl; arr_size > 0; arr_size -= vl, foreground += vl, background += vl, dst += vl)
+    {
+        vl = __riscv_vsetvl_e32m4(arr_size);
+        vuint16m2_t v_u16_foreground = __riscv_vwcvtu_x_x_v_u16m2(__riscv_vle8_v_u8m1(foreground, vl), vl);
+        vuint16m2_t v_u16_background = __riscv_vwcvtu_x_x_v_u16m2(__riscv_vle8_v_u8m1(background, vl), vl);
+
+        vfloat32m4_t v_f32_foreground = __riscv_vfwcvt_f_xu_v_f32m4(v_u16_foreground, vl);
+        vfloat32m4_t v_f32_background = __riscv_vfwcvt_f_xu_v_f32m4(v_u16_background, vl);
+
+        v_f32_foreground = __riscv_vfmul_vf_f32m4(v_f32_foreground, alpha, vl);
+        vfloat32m4_t v_f32_dst = __riscv_vfmacc_vf_f32m4(v_f32_foreground, 1.0f - alpha, v_f32_background, vl);
+
+        vuint16m2_t v_u16_dst = __riscv_vfncvt_xu_f_w_u16m2(v_f32_dst, vl);
+        __riscv_vse8_v_u8m1(dst, __riscv_vncvt_x_x_w_u8m1(v_u16_dst, vl), vl); // v_u16_dst -> v_u8_dst and save in dst
+    }
 }
--- perf/src/alpha_compositing.cpp
+++ perf/src/alpha_compositing.cpp
@@ -1,5 +1,73 @@
 #include "perf_common.hpp"
 
+#if defined(scalar)
+#define MOD scalar
+#define ALPHA_COMPOSITING_FUNC alpha_compositing_scalar
+#elif defined(rvv)
+#define MOD rvv
+#define ALPHA_COMPOSITING_FUNC alpha_compositing_rvv
+#else
+#error "Tested function is not defined"
+#endif
+
+static inline void bench_alpha_compositing(int width, int height, int channels, int iterations) {
+    int length = width * height * channels;
+    std::vector<uint8_t> foreground(length, 0);
+    std::vector<uint8_t> background(length, 0);
+    std::vector<uint8_t> dst(length, 0);
+    float alpha = 0.5f;
+
+    // prepareImage(foreground.data(), width, height, channels);
+    // prepareImage(background.data(), width, height, channels);
+
+    volatile uint64_t start = 0,
+                    end = 0,
+                    totalCycles = 0,
+                    minCycles = std::numeric_limits<uint64_t>::max(),
+                    diff;
+
+    for(int i = 0; i < iterations; i++){
+        start = rdcycle();
+        ALPHA_COMPOSITING_FUNC(foreground.data(), background.data(), alpha, dst.data(), width, height, channels);
+        end = rdcycle();
+        diff = end - start;
+        totalCycles += diff;
+        if (diff < minCycles)
+            minCycles = diff;
+    }
+#if defined(scalar)
+    std::string funcName = std::string{"Scalar ALPHA_COMPOSITING with arr length "} + std::to_string(length);
+#elif defined(rvv)
+    std::string funcName = std::string{"RVV ALPHA_COMPOSITING with arr length "} + std::to_string(length);
+#else
+#error "Tested function is not defined"
+#endif
+
+    printStat(minCycles, totalCycles, iterations, length, funcName);
+}
+
 int main() {
+
+    //
+    // Bench small sizes
+    //
+    constexpr int small_width = 10;
+    constexpr int small_iterations = 10'000;
+    bench_alpha_compositing(small_width, small_width, small_width, small_iterations);
+
+    //
+    // Bench medium sizes
+    //
+    constexpr int medium_length = 50;
+    constexpr int medium_iterations = 1'000;
+    bench_alpha_compositing(medium_length, medium_length, medium_length, medium_iterations);
+
+    //
+    // Bench big sizes
+    //
+    constexpr int big_length = 100;
+    constexpr int big_iterations = 100;
+    bench_alpha_compositing(big_length, big_length, big_length, big_iterations);
+
     return 0;
 }
